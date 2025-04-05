e571f0d1a0de876b19f8eb76f3964ab2f6c91955
ambadi365 <ambadims365@gmail.com>
2025-04-05 11:31:06 +0530

    first commit

---
+++ server/helpers/cloudinary.js
@@ -0,0 +1,39 @@
+const cloudinary = require("cloudinary").v2;
+
+//configure with env data
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,   // Replace with your Cloudinary cloud name
+  api_key:process.env.API_KEY,        // Replace with your API key
+  api_secret: process.env.API_SECRET   // Replace with your API secret
+});
+
+
+const uploadMediaToCloudinary = async (filePath) => {
+  try {
+    const result = await cloudinary.uploader.upload(filePath, {
+      resource_type: "auto",
+    });
+
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error uploading to cloudinary");
+  }
+};
+
+const deleteMediaFromCloudinary = async (publicId) => {
+  try {
+    await cloudinary.uploader.destroy(publicId);
+  } catch (error) {
+    console.log(error);
+    throw new Error("failed to delete assest from cloudinary");
+  }
+};
+
+module.exports = { uploadMediaToCloudinary, deleteMediaFromCloudinary };
