0b921729f8c9e1e2ddcbb31ff531aa0425cc8ed0
Anandhu  M S <88383366+anandhums977@users.noreply.github.com>
2025-05-12 13:15:25 +0530

    Update axiosInstance.js

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://edu-nexus-nt7o.onrender.com",
 });
 
 axiosInstance.interceptors.request.use(
